921aae28cc3a4375549467a971768b38e52a91f0
Nina Jade <jaderiver64@gmail.com>
2021-04-12 23:14:07 -0500

    adding styles to 404 error page

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,6 @@ const apiRoutes = require("./api");
 
 router.use("/api", apiRoutes);
 
-router.use((req, res) => res.status(404).send("<h1>404 - Not Found Error</h1>"));
+router.use((req, res) => res.status(404).send("<div style='text-align:center;'><div style='color: green; font-weight: bold;'><h1><br/>404 Error...</h1></br><span style='font-size:100px; '>&#127921;</span><br/><h4>This page isn't doing anything today.</h4></div></div>"));
 
 module.exports = router;
